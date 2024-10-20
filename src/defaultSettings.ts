bd42578b85d25618db91ecf671652252b5e0805c
dinamik567 <irhin2011@yandex.ru>
2024-10-21 01:52:25 +0300

    Add schedule page

---
+++ src/defaultSettings.ts
@@ -0,0 +1,18 @@
+export enum NAME_OF_DAY {
+  MONDAY = "Понедельник",
+  TUESDAY = "Вторник",
+  WEDNESDAY = "Среда",
+  THURSDAY = "Четверг",
+  FRIDAY = "Пятница",
+  SATURDAY = "Суббота",
+  SUNDAY = "Воскресенье",
+}
+
+export enum CLASS_MODIFIERS {
+  ORANGE = "headerDay_orange",
+  YELLOW = "headerDay_yellow",
+  GREEN = "headerDay_green",
+  LIGHT_BLUE = "headerDay_lightblue",
+  BLUE = "headerDay_blue",
+  PURPLE = "headerDay_purple",
+}
